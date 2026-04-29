181b1245854b4ab77f2bec2b8b728765572e945c
Ajit Raj <amitraj231226@gmail.com>
2026-04-29 12:47:07 +0530

    Added V2 reporting system

--- scanner.py
+++ scanner.py
@@ -2,7 +2,7 @@ import re
 
 
 def detect_type(target):
-    if "@" in target:
+    if re.match(r"[^@]+@[^@]+\.[^@]+", target):
         return "Email"
     elif "." in target:
         return "Domain"
@@ -14,10 +14,10 @@ def calculate_risk(target):
     risks = []
 
     if len(target) < 8:
-        risks.append("Target is too short")
+        risks.append("Too short")
 
     if target.islower():
-        risks.append("No uppercase characters")
+        risks.append("No uppercase letters")
 
     if target.isalpha():
         risks.append("No numbers or symbols")
@@ -25,20 +25,39 @@ def calculate_risk(target):
     return risks
 
 
+def save_report(target, target_type, score, risks):
+    with open("report.txt", "w") as file:
+        file.write("DIGITAL FOOTPRINT REPORT\n")
+        file.write("=" * 30 + "\n")
+        file.write(f"Target: {target}\n")
+        file.write(f"Type: {target_type}\n")
+        file.write(f"Risk Score: {score}/100\n\n")
+
+        if risks:
+            file.write("Risks:\n")
+            for risk in risks:
+                file.write(f"- {risk}\n")
+        else:
+            file.write("No risks detected.\n")
+
+
 def analyze(target):
     target_type = detect_type(target)
-
-    print(f"\nTarget Type: {target_type}")
 
     risks = calculate_risk(target)
 
     score = max(100 - (len(risks) * 20), 0)
 
+    print(f"\nTarget Type: {target_type}")
     print(f"Risk Score: {score}/100\n")
 
     if risks:
-        print("Potential Risks Found:")
+        print("Potential Risks:")
         for risk in risks:
             print(f"[!] {risk}")
     else:
         print("No obvious risks detected.")
+
+    save_report(target, target_type, score, risks)
+
+    print("\nReport saved as report.txt")
